80d9b236348ccb2c76a04fae9a3ca7f1066eb71c
github username <github email address>
2019-02-06 08:58:40 -0500

    fill out all models, migrations, controllers, views

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,11 @@
 class DogsController < ApplicationController
+
+  def index
+    @dogs = Dog.all
+    @sorted = @dogs.sort_by {|d| d.employee_count}
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
 end
